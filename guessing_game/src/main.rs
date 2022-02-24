f40e8d09c102b3fa6091cdb836106b9bec7e5b5f
Fish <bingyuzhang@trip.com>
2022-02-24 15:33:28 +0800

    ✨ feat: refine error handling

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,13 +8,17 @@ fn main() {
     println!("Secret Number: {}", secret_number);
 
     loop {
-        println!("Guess the number!");
+        println!("Please input your guess.");
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
         println!("You guessed: {}", guess);
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
