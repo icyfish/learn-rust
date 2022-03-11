dacb966602d255f0a4d8ff49aa4975d639948483
icyfish <icyyfish@gmail.com>
2022-03-11 18:22:19 +0800

    exercises

---
+++ exercises/src/main.rs
@@ -0,0 +1,9 @@
+
+// Convert temperatures between Fahrenheit and Celsius.
+// Generate the nth Fibonacci number.
+// Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” taking advantage of the repetition in the song.
+fn main() {
+    println!("Hello, world!");
+}
+
+
